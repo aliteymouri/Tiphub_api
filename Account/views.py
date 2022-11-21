680a6f934c25c09a685219c5668ff27087fb2e68
aliteymouri <aliteymr@gmail.com>
2022-11-21 16:37:58 +0330

    Update views.py

--- Account/views.py
+++ Account/views.py
@@ -1,9 +1,9 @@
-from django.contrib.auth import logout
 from rest_framework.permissions import IsAuthenticated
 from rest_framework.generics import ListAPIView
 from rest_framework.response import Response
 from rest_framework.views import APIView
 from .serializers import UserSerializer
+from django.contrib.auth import logout
 from rest_framework import status
 from .models import User
 
